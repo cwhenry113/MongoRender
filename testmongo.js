71506c8bedb6cf08c61f5e8234f75deaad011ca2
cwhenry113 <69873552+cwhenry113@users.noreply.github.com>
2023-03-28 11:22:09 -0500

    Update2

--- testmongo.js
+++ testmongo.js
@@ -36,7 +36,7 @@ console.log("Looking for: " + searchKey);
 async function run() {
   try {
     const database = client.db('test');
-    const parts = database.collection('myTable');
+    const parts = database.collection('myTable2');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
